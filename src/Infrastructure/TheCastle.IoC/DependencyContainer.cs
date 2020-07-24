92883ed87c7c9ae9f364bd3430a6d9ea0e365c83
Renaud Simon <48937051+Renaud1234@users.noreply.github.com>
2020-07-24 17:50:52 +0100

    Migration Identity

---
+++ src/Infrastructure/TheCastle.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureDemo.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // CleanArchitecture.Application
+            //services.AddScoped<IBookService, BookService>();
+
+            // CleanArchitecture.Domain.Interfaces | CleanArchitecture.Infra.Data.Repositories
+            //services.AddScoped<IBookRepository, BookRepository>();
+        }
+    }
+}
