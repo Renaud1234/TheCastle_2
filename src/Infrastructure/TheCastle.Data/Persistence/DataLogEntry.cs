3c32c5ccbddc5d90f4ce91cae14be9805c39c55e
Renaud Simon <48937051+Renaud1234@users.noreply.github.com>
2020-07-28 07:53:36 +0100

    Data log manager

---
+++ src/Infrastructure/TheCastle.Data/Persistence/DataLogEntry.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TheCastle.Domain.Entities;
+
+namespace TheCastle.Data.Persistence
+{
+    class DataLogEntry
+    {
+        public DataLogEntry(EntityEntry entry)
+        {
+            Entry = entry;
+        }
+
+        public EntityEntry Entry { get; }
+        public string TableName { get; set; }
+        public string ActionType { get; set; }
+        public Dictionary<string, object> KeyValues { get; } = new Dictionary<string, object>();
+        public Dictionary<string, object> OldValues { get; } = new Dictionary<string, object>();
+        public Dictionary<string, object> NewValues { get; } = new Dictionary<string, object>();
+        public List<PropertyEntry> TemporaryProperties { get; } = new List<PropertyEntry>();
+
+        public bool HasTemporaryProperties => TemporaryProperties.Any();
+
+        public DataLog ToDataLog()
+        {
+            var dataLog = new DataLog()
+            {
+                TableName = TableName,
+                ActionType = ActionType,
+                ActionDateTime = DateTime.UtcNow,
+                RecordId = JsonConvert.SerializeObject(KeyValues),
+                OriginalValue = OldValues.Count == 0 ? null : JsonConvert.SerializeObject(OldValues),
+                NewValue = NewValues.Count == 0 ? null : JsonConvert.SerializeObject(NewValues)
+            };
+
+            return dataLog;
+        }
+    }
+}
